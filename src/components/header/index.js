23f45b70f89e8f1f572c6f76d729a03d2e9bd48a
GNG <GNG@Guilhermes-MacBook-Pro.local>
2020-09-04 12:50:44 -0300

    update cookies

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
+import Cookies from 'universal-cookie';
 
+const cookies = new Cookies();
+const cookies_options =  { domain: '.evaluatz.com', path: '/' };
+
+
+const TOKEN_KEY = 'jwtToken';
+const USER_INFO = 'userInfo';
 
 function Header (){
   const [isLogged, setIsLogged] = useState(false);
   const [user, setUser] = useState(null);
   useEffect(() => {
-  
+    if (cookies && cookies.get(USER_INFO, cookies_options)) {
+      setUser(cookies.get(USER_INFO, cookies_options) || null);
+    }
   });
 
 
@@ -19,7 +28,7 @@ function Header (){
         <div className="evaluatz_header_right d-flex align-items-center justify-content-between">
           {isLogged ?
           <div className="evaluatz_signin_btn"  >{user}</div>:
-          <a href="https://accounts.evaluatz.com/auth/login?">
+          <a href="https://accounts.evaluatz.com/auth/login?source=esports">
             <div className="evaluatz_signin_btn" >Sign Up</div> 
           </a>
            
